4958b01da5a9a6379a2dde54ede33369a4c1e33e
walid-haider <68023398+walid-haider@users.noreply.github.com>
2020-07-08 19:02:58 +0200

    Delete UserRepository.java

--- UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.tacticamz.javaflux.security2;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-
-    User findByUsername(String username);
-}
